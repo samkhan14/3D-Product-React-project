4758c522c8d1d2e817c92449e132d78997ef320b
Sumaim Ahmed <sumaimahmed@gmail.com>
2023-08-17 17:34:59 +0500

    boiler plate has been created for this project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <h1 className='text-3xl font-bold underline'>Hello React</h1>
+    </>
+  )
+}
+
+export default App
